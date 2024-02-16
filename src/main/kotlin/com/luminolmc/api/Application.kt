bc28377e65f8254ffa4de87c8cf3682dc63f04ef
Klop233 <3337913379@qq.com>
2024-02-16 12:19:40 +0800

    finish projects route

--- src/main/kotlin/com/luminolmc/api/Application.kt
+++ src/main/kotlin/com/luminolmc/api/Application.kt
@@ -3,14 +3,16 @@ package com.luminolmc.api
 import com.luminolmc.api.routing.api.loadProjectsRoute
 import com.luminolmc.api.routing.loadMiscRoute
 import freemarker.cache.ClassTemplateLoader
+import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 
+
 fun main() {
-    embeddedServer(Netty, port = 5555, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 7878, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
@@ -24,7 +26,7 @@ fun Application.installPlugins() {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
     }
     install(ContentNegotiation) {
-
+        gson {  }
     }
 }
 
