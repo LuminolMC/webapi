665de88d029557962fe77db1241ff1b2415d2838
ShintoKosei <2081384668@qq.com>
2024-02-19 01:13:00 +0800

    update

--- src/main/kotlin/com/luminolmc/api/routing/api/Projects.kt
+++ src/main/kotlin/com/luminolmc/api/routing/api/Projects.kt
@@ -6,6 +6,18 @@ import io.ktor.server.routing.*
 
 fun Application.loadProjectsRoute() {
     routing {
+        route("/v1/projects") {
+            get {
+                var response = listOf(
+                    mapOf(
+                        "id" to "luminol",
+                        "name" to "Luminol",
+                        "repo" to "LuminolMC/Luminol"
+                    )
+                )
+                call.respond(response)
+            }
+        }
         route("/v1/projects/luminol") {
             get {
                 var response = mapOf(
@@ -18,15 +30,14 @@ fun Application.loadProjectsRoute() {
                 call.respond(response)
             }
         }
-
-        route("/v1/projects") {
+        route("/v1/projects/lightingluminol") {
             get {
-                var response = listOf(
-                    mapOf(
-                        "id" to "luminol",
-                        "name" to "Luminol",
-                        "repo" to "LuminolMC/Luminol"
-                    )
+                var response = mapOf(
+                    "code" to 200,
+                    "project_id" to "lightingluminol",
+                    "project_name" to "LightingLuminol",
+                    "version_groups" to listOf("1.20"),
+                    "versions" to listOf("1.20.4")
                 )
                 call.respond(response)
             }
