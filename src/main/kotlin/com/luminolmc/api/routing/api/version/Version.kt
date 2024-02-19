0e2a532ddc98ac0a1f4d7ff59b458c200fb567cf
Klop233 <3337913379@qq.com>
2024-02-19 11:17:42 +0800

    Goodbye Holiday :(

--- src/main/kotlin/com/luminolmc/api/routing/api/version/Version.kt
+++ src/main/kotlin/com/luminolmc/api/routing/api/version/Version.kt
@@ -1,5 +1,6 @@
 package com.luminolmc.api.routing.api.version
 
+import com.luminolmc.api.data.basic
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
