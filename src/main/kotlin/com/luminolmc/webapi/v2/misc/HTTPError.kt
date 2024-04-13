f53f9ab3e116d15f97111ec8d7b812f4eebcbd40
Klop233 <3337913379@qq.com>
2024-04-13 14:24:36 +0800

    update

--- src/main/kotlin/com/luminolmc/webapi/v2/misc/HTTPError.kt
+++ src/main/kotlin/com/luminolmc/webapi/v2/misc/HTTPError.kt
@@ -5,7 +5,7 @@ enum class HTTPError(
     private val message: String
 ) {
     NOT_FOUND(404, "Not found"),
-    FORBIDDEN(403, "你妈的别试了"),;
+    FORBIDDEN(403, "Forbidden"),;
 
     override fun toString(): String {
         return message
